85731e2891bc55bf31abf7d821ea4cd77539988c
spquor <spquor@gmail.com>
2025-04-19 22:52:31 +0300

    Fix: do_outb shoud not return a value

--- src/rtapi/uspace_xenomai.cc
+++ src/rtapi/uspace_xenomai.cc
@@ -153,9 +153,7 @@ struct XenomaiApp : RtapiApp {
 
     void do_outb(unsigned char val, unsigned int port) {
 #ifdef HAVE_SYS_IO_H
-        return outb(val, port);
-#else
-        return 0;
+        outb(val, port);
 #endif
     }
 
